b757a003a9edd1eebfd749d1552741a9f6bb9b8d
Om Balapure <odbalapure@gmail.com>
2025-01-21 08:52:24 +0530

    Simple interface example

---
+++ 04.interfaces/shapes/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+)
+
+type shape interface {
+	getArea() float64
+}
+
+type triangle struct {
+	base   float64
+	height float64
+}
+
+type square struct {
+	length float64
+}
+
+func (s square) getArea() float64 {
+	return float64(s.length * s.length)
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+
+func getAllTypesOfArea(s shape) {
+	fmt.Println("getAllTypesOfArea::Area", s.getArea())
+}
+
+func main() {
+	sq := square{
+		length: 10.5,
+	}
+	tr := triangle{
+		base:   10.2,
+		height: 23.3,
+	}
+
+	shapes := []shape{tr, sq}
+	for _, sh := range shapes {
+		fmt.Println("getArea::Area:", sh.getArea())
+	}
+
+	getAllTypesOfArea(sq)
+	getAllTypesOfArea(tr)
+}
